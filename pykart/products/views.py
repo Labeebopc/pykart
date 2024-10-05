6d9b99e0828dd2cb103f65bed48733d40a71e6ed
Labeeb OPC <Labeebopc313@gmail.com>
2024-10-05 18:56:05 +0400

    template designs

--- pykart/products/views.py
+++ pykart/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Product page
+
+def index(request):
+    return render(request, 'index.html')
+
+def list_products(request):
+    return render(request, 'products.html')
+
+def detail_products(request):
+    return render(request, 'product_details.html')
