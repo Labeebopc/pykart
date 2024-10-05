a58c29bc0fca66f69c45321b3e98a52e828aeca2
Labeeb OPC <Labeebopc313@gmail.com>
2024-10-05 15:33:53 +0400

    models created and migrated

--- pykart/customers/models.py
+++ pykart/customers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Signup models
+
+class Customer(models.Model):
+    # To move into trash
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = ((LIVE, 'Live'), (DELETE, "Delete"))
+
+    name = models.CharField(max_length = 200)
+    address = models.TextField()
+    user = models.OneToOneField(User, on_delete = models.CASCADE , related_name = 'customer_profile')
+    phone = models.CharField(max_length = 10)
+    delete_status = models.IntegerField(choices = DELETE_CHOICES, default = LIVE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    # __str__ , string reprecentation
+    def __str__(self) -> str:
+        return self.title
