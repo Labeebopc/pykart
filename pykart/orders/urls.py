5b767fb6ff8eeb983c80fbc99e78465ad99b4eb1
Labeeb OPC <Labeebopc313@gmail.com>
2024-10-06 11:07:15 +0400

    layout designs completed

---
+++ pykart/orders/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+
+
+urlpatterns = [
+    path('cart', views.show_cart, name='cart'),
+]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
