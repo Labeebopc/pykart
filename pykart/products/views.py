3647735e3ed70a96a3674d3a9ab4a29c0d4c88ee
Labeeb OPC <Labeebopc313@gmail.com>
2024-10-06 13:34:16 +0400

    product module: product listing, pagination etc. completed

--- pykart/products/views.py
+++ pykart/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from . models import Product
+from django.core.paginator import Paginator
 
 # Product page
 
@@ -6,7 +8,25 @@ def index(request):
     return render(request, 'index.html')
 
 def product_list(request):
-    return render(request, 'products.html')
+
+    # To calculate requested page
+    page = 1
+    if request.GET:
+        page = request.GET.get("page", 1)
+
+    # Fetch products
+    fetchProducts = Product.objects.all()
+
+    # Pagination
+    product_paginator = Paginator(fetchProducts, 2)
+
+    fetchProducts = product_paginator.get_page(page)
+
+    # Create a dictionary
+    context = {
+        "products" : fetchProducts
+    }
+    return render(request, 'products.html', context)
 
 def product_details(request):
     return render(request, 'product_details.html')
